edb69021e03825fe082b6d08c3db942af313c390
huobingqian <huobingqian@yuanfudao.com>
2021-09-04 19:57:49 +0800

    Initial Commit

---
+++ hello-swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello-swift
+//
+//  Created by benny on 2021/9/4.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
